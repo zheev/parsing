84f71fc90122a8b3c0bdd61415d0e3fcba1dafb0
zheev <kz.zhebrikov@yandex.ru>
2019-01-22 23:43:01 +0300

    Change files

--- players.php
+++ players.php
@@ -18,28 +18,30 @@ require $_SERVER['PWD'].'/lib.php';
 require $_SERVER['PWD'].'/constants.php';
 require $_SERVER['PWD'].'/db.php';
 
+//получим html код списка игроков и тренеров
 $html = getHtmlPlayers();
-
+//убираем лишнее из html кода, осавляем общий блок со списком
 preg_match('/<section class="main">(.*?)<\/section>/ms', $html, $match);
-
+//запишем в переменную html код. Чтобы очистить массив и не хранить в памяти
 $filterHtml = $match[1];
-
+//удаляем массив
 unset($match);
-
+// получаем имя и фамилию игроков
 preg_match_all('/<div class="text">(.*?)<\/div>/ms', $filterHtml, $players);
-
+//запишем в переменную, чтобы очистить массив
 $playersHtml = $players[1];
-
+//удаляем массив
 unset($players);
-
+//обяъявим переменную  как массив
 $playerFamaly = [];
-
+//циклом проходим по всем полученным игрокам и тренерам
 foreach ($playersHtml as $player)
 {
+    //удаляем повторяющие пробелы и заменяем их на 1
     $playerData = preg_replace('/\s{2,}/',' ', strip_tags($player));
+    //строку делим на пробелы и записываем фамилии
     $playerFamaly[] = explode(' ', $playerData)[1].PHP_EOL;
 }
 
-//addPlayer($playerFamaly);
-
-print_r(selectAllPlaeyrs());
+//запишем фаимилии в бд
+addPlayer($playerFamaly);
